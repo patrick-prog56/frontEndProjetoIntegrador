9c239e57582457d1ab6f08c5c403d1f5fefe0d6b
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-06-28 17:13:39 -0300

    merge

--- girlsCamp/src/app/app-routing.module.ts
+++ girlsCamp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path:'', redirectTo: 'inicio',pathMatch: 'full'},
