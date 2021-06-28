6e1c8e2b0fbcf6fcb7b0f40ead7e15740e8afa3b
gilsonmirandaj <gilsonmirandajunior2@gmail.com>
2021-06-28 16:20:53 -0300

    add sobre

--- girlsCamp/src/app/app.module.ts
+++ girlsCamp/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule,
