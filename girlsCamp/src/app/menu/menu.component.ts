8e7060a9aebe3fcf82c89f6fda4adde4cbf1986d
Carolina <ribeiro.carolribeiro.carolina@gmail.com>
2021-07-14 00:00:08 -0300

    Fazendo uns ajustes

--- girlsCamp/src/app/menu/menu.component.ts
+++ girlsCamp/src/app/menu/menu.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
 import { AuthService } from '../service/auth.service';
+import { PostagemService } from '../service/postagem.service';
 
 @Component({
   selector: 'app-menu',
@@ -9,9 +11,17 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./menu.component.css'],
 })
 export class MenuComponent implements OnInit {
+
+  palavra: string
+
+  listaPostagem: Postagem[];
+  postagem: Postagem = new Postagem();
+
   constructor(
     public auth: AuthService, 
-    private router: Router) {}
+    private router: Router,
+    private postagemService: PostagemService
+    ) {}
 
   ngOnInit() {}
 
@@ -21,5 +31,27 @@ export class MenuComponent implements OnInit {
     environment.token = '';
     environment.nome = '';
     environment.id = 0;
+  }
+
+  getAll(){
+    this.postagemService.getAll().subscribe((resp: Postagem[]) => {
+      this.listaPostagem = resp
+    })
+  }
+
+  findByIdPostagem(id: number){
+    this.postagemService.getByIdPostagem(id).subscribe((resp: Postagem) => {
+      this.postagem = resp
+    })
+  }
+
+  findByConteudoPostagem(){
+    if(this.palavra == ''){
+      this.getAll()
+    }else{
+      this.postagemService.getByConteudoPostagem(this.palavra).subscribe((resp: Postagem[]) => {
+        this.listaPostagem = resp
+      })
+    }
   }
 }
